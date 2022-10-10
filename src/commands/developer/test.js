bca535dd5e78f62984053863bc20eafc9e8afe83
chamberrr2809 <chamberlynsiagian@candletreeschool.sch.id>
2022-10-10 20:19:54 +0700

    Initial commit

---
+++ src/commands/developer/test.js
@@ -0,0 +1,23 @@
+const { ApplicationCommandOptionType } = require('discord.js');
+const { ChatInputCommand } = require('../../classes/Commands');
+
+module.exports = new ChatInputCommand({
+  permLevel: 'Developer',
+  data: {
+    description: 'Test command for the developers',
+    options: [
+      {
+        name: 'value',
+        description: 'input',
+        type: ApplicationCommandOptionType.String,
+        required: true
+      }
+    ],
+    default_member_permissions: 0 // Unavailable to non-admins in guilds
+  },
+
+  run: (client, interaction) => {
+    // ...
+  }
+});
+
